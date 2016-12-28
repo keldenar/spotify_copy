bff9a0b0645ddd3aab59f8a9f2b5fe9ec000ce2a
Bret Combast <bret_combast@ultimatesoftware.com>
2016-12-28 14:30:51 -0500

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    /**
+     * Logs into spotify and gets an auth token
+     */
+    public function login1()
+    {
+        
+    }
+}
