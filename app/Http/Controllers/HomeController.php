be6891da0bb37c1ca35ca790ed9ecee795710ac9
Bret Combast <bret_combast@ultimatesoftware.com>
2016-12-29 14:55:15 -0500

    Dump

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,8 +42,8 @@ class HomeController extends Controller
      */
     public function auth1(Request $request, Response $response)
     {
-        $full_req = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
-        dump($request, $response, $full_req->getQueryString());
+
+        dump($request->fullUrl(), $response);
 
     }
 }
